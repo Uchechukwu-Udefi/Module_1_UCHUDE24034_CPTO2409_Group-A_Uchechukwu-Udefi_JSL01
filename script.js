ee4c3d9811947ace58fb54f16653784c06c8d9e2
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-01-15 11:02:44 +0200

    Update validation logic

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    //let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
+
+       let result = ""
+       if (result === "pet_" + result === "0-9" + result === "a-z") {
+           result = "Valid Syntax";
+       } else {
+           result = "Invalid Syntax";
+       }
+            
             document.getElementById('result').innerText = result;
 }
 
