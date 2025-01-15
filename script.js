8a868c2dc84ca876d9a7d93e45562ea299b0f1e0
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-01-15 12:10:31 +0200

    Update input match to regex

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ function validateSyntax() {
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
+        let result = "";
+        let regex = /^pet_\d{4}[A-Za-z]{1,30}$/;
 
-        let result = ""
-        let regex = /^pet_\d{4}[a-zA-Z]{1,30}$/;
+    // Check if the input matches the regex
+        if (regex.test(input)) {
+            result = "Valid Syntax";
+        } else {
+            result = "Invalid Syntax";
+        }
 
-       
+    // Display the result
         document.getElementById('result').innerText = result;
 }
 
